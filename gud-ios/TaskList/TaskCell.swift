af77018341cc8d1bd49791992dae050c10c5e664
sudofluff <sudofluff@icloud.com>
2019-09-11 23:01:06 -0700

    Uses stackView to organize views for vertical design

--- gud-ios/TaskList/TaskCell.swift
+++ gud-ios/TaskList/TaskCell.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class TaskCell: UITableViewCell, UniquelyIdentifable {
-  private var IMAGE_DATA_VIEW_SIZE = CGSize(width: 44, height: 0)
-  
   private var imageViewHeightConstraint: NSLayoutConstraint!
   
   lazy private var containerView: UIView = {
@@ -22,7 +20,6 @@ class TaskCell: UITableViewCell, UniquelyIdentifable {
   
   lazy private var completionIndicatorView: UIView = {
     let view = UIView()
-    view.backgroundColor = .red
     view.translatesAutoresizingMaskIntoConstraints = false
     return view
   }()
@@ -40,16 +37,26 @@ class TaskCell: UITableViewCell, UniquelyIdentifable {
   
   lazy private var dateLabel: UILabel = {
     let label = UILabel()
+    label.backgroundColor = CustomColor .clear
     label.font = UIFont.preferredFont(forTextStyle: .footnote)
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
   
+  lazy private var stackView: UIStackView = {
+    let view = UIStackView(arrangedSubviews: [self.imageDataView, self.dateLabel])
+    view.spacing = 4
+    view.alignment = .leading
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.axis = .vertical
+    return view
+  }()
+  
   lazy private var imageDataView: UIImageView = {
     let view = UIImageView()
     view.translatesAutoresizingMaskIntoConstraints = false
-    view.backgroundColor = .orange
-    view.contentMode = .scaleAspectFill
+    view.backgroundColor = CustomColor.clear
+    view.contentMode = .scaleAspectFit
     view.clipsToBounds = true
     return view
   }()
@@ -64,33 +71,39 @@ class TaskCell: UITableViewCell, UniquelyIdentifable {
   private func configureView() {
     self.contentView.backgroundColor = .clear
     self.selectionStyle = .none
+    
     self.contentView.addSubview(self.containerView)
     self.containerView.addSubview(self.titleTextView)
     self.containerView.addSubview(self.separatorView)
     self.containerView.addSubview(self.dateLabel)
     self.containerView.addSubview(self.imageDataView)
     self.containerView.addSubview(self.completionIndicatorView)
+    self.containerView.addSubview(self.stackView)
     self.containerView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
     self.containerView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor).isActive = true
     self.containerView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
     self.containerView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor).isActive = true
+    
     self.completionIndicatorView.topAnchor.constraint(equalTo: self.containerView.topAnchor).isActive = true
     self.completionIndicatorView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
     self.completionIndicatorView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor).isActive = true
     self.completionIndicatorView.widthAnchor.constraint(equalToConstant: 5).isActive = true
+    
     self.titleTextView.topAnchor.constraint(equalTo: self.containerView.topAnchor, constant: 8).isActive = true
     self.titleTextView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor, constant: 16).isActive = true
     self.titleTextView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor, constant: -16).isActive = true
     self.titleTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 22).isActive = true
-    self.titleTextView.bottomAnchor.constraint(equalTo: self.imageDataView.topAnchor, constant: -8).isActive = true
-    self.imageDataView.bottomAnchor.constraint(equalTo: self.dateLabel.topAnchor, constant: -8).isActive = true
-    self.imageDataView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor, constant: 16).isActive = true
-    self.imageDataView.heightAnchor.constraint(equalToConstant: self.IMAGE_DATA_VIEW_SIZE.height).isActive = true
-    self.imageDataView.widthAnchor.constraint(equalToConstant: self.IMAGE_DATA_VIEW_SIZE.width).isActive = true
-    self.dateLabel.leftAnchor.constraint(equalTo: self.containerView.leftAnchor, constant: 16).isActive = true
+    self.titleTextView.bottomAnchor.constraint(equalTo: self.stackView.topAnchor, constant: -4).isActive = true
+    
+    self.stackView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor, constant: 16).isActive = true
+    self.stackView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor, constant: -16).isActive = true
+    self.stackView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -8).isActive = true
+    
+    self.imageDataView.heightAnchor.constraint(equalToConstant: 256).isActive = true
+    self.imageDataView.widthAnchor.constraint(equalToConstant: 256).isActive = true
+    
     self.dateLabel.heightAnchor.constraint(equalToConstant: 22).isActive = true
-    self.dateLabel.rightAnchor.constraint(equalTo: self.containerView.rightAnchor, constant: -16).isActive = true
-    self.dateLabel.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -16).isActive = true
+    
     self.separatorView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor, constant: 16).isActive = true
     self.separatorView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
     self.separatorView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor).isActive = true
@@ -99,17 +112,10 @@ class TaskCell: UITableViewCell, UniquelyIdentifable {
   
   func configure(task: Task) {
     self.titleTextView.text = task.title
-    self.dateLabel.text = task.createdAtFormattedString
+    self.dateLabel.text = "Created at: " + task.createdAtFormattedString
     self.completionIndicatorView.backgroundColor = task.isCompleted ? CustomColor.seaweedGreen : CustomColor.mandarinOrange
-    UIView.animate(withDuration: 1, animations: {
-      if let data = task.imageData {
-        self.imageDataView.image = UIImage(data: data)
-        self.IMAGE_DATA_VIEW_SIZE.height = 44
-      } else {
-        self.imageDataView.image = nil
-        self.IMAGE_DATA_VIEW_SIZE.height = 0
-      }
-    }) { (completed) in
+    self.imageDataView.isHidden = task.imageData == nil ? true : false
+    UIView.animate(withDuration: 1) {
       self.layoutIfNeeded()
     }
   }
